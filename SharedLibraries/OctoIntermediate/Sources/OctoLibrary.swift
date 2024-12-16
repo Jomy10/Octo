720a3b22f7e6de9e4d0cd681a3f5e6fd3c104c4b
Jomy10 <jonas.vbs4@gmail.com>
2024-12-16 15:01:25 +0100

    typedefs now defined in Library

--- SharedLibraries/OctoIntermediate/Sources/OctoLibrary.swift
+++ SharedLibraries/OctoIntermediate/Sources/OctoLibrary.swift
@@ -8,9 +8,19 @@ public struct OctoLibrary: AutoRemovable {
   public var ffiLanguage: Language = .c
   private var langRefMap: [AnyHashable:Int] = [:]
   private var nameMap: [String:Int] = [:]
+  private var typedefs: [String:OctoType] = [:]
   public private(set) var objects: [OctoObject] = []
   public var destroy: () -> Void = {}
   private var hasFinalized = false
+
+  //public var objectInclude: (OctoObject) -> Bool = { (object: OctoObject) in true }
+  // TODO: provide includes for C?
+
+  @available(*, deprecated)
+  public func includedObjects() -> some Sequence<OctoObject> {
+    //self.objects.filter(self.objectInclude)
+    self.objects
+  }
 
   public init() {}
 
@@ -33,6 +43,17 @@ public struct OctoLibrary: AutoRemovable {
     }
     self.langRefMap[ref] = self.objects.count
     self.objects.append(obj)
+    //var resolved: [Int] = []
+    //for (i, resolution) in self.deferredTypedefResolutions.enumerated() {
+    //  let (typedefName, typeResolution) = resolution
+    //  if let resolvedType = typeResolution(self) {
+    //    self.addTypedef(toType: resolvedType, name: typedefName)
+    //    resolved.append(i)
+    //  }
+    //}
+    //for resolvedIndex in resolved.reversed() {
+    //  self.deferredTypedefResolutions.remove(at: resolvedIndex)
+    //}
   }
 
   public func getObject(forRef ref: AnyHashable) -> OctoObject? {
@@ -43,11 +64,72 @@ public struct OctoLibrary: AutoRemovable {
   }
 
   public func getObject(byName name: String) -> OctoObject? {
+    if let typedefType = self.typedefs[name] {
+      switch (typedefType.kind) {
+        case .Record(let record): return record
+        case .Enum(let e): return e
+        default:
+          Self.logger.warning("Found \(typedefType) for typedef \(name), continuing to search for object named \(name)")
+      }
+    }
+
     guard let id = self.nameMap[name] else {
       return nil
     }
     return self.objects[id]
   }
+
+  public func getType(byName name: String) -> OctoType? {
+    if let typedefType = self.typedefs[name] {
+      return typedefType
+    }
+
+    guard let id = self.nameMap[name] else {
+      return nil
+    }
+    let obj = self.objects[id]
+    if let obj = obj as? OctoEnum {
+      return OctoType(kind: .Enum(obj), optional: false, mutable: true)
+    } else if let obj = obj as? OctoRecord {
+      return OctoType(kind: .Record(obj), optional: false, mutable: true)
+    } else {
+      fatalError("bug")
+    }
+  }
+
+  enum AddTypedefError: Error {
+    case typeNotFound(ref: AnyHashable)
+    case typeNotTypedefCapable(obj: OctoObject)
+  }
+
+  public mutating func addTypedef(toRef ref: AnyHashable, name: String) throws {
+    if let obj = self.getObject(forRef: ref) {
+      try self.addTypedef(toObject: obj, name: name)
+    } else {
+      throw AddTypedefError.typeNotFound(ref: ref)
+    }
+  }
+
+  public mutating func addTypedef(toObject object: OctoObject, name: String) throws {
+    if let obj = object as? OctoRecord {
+      self.addTypedef(toType: OctoType(kind: .Record(obj), optional: false, mutable: true), name: name)
+    } else if let obj = object as? OctoEnum {
+      self.addTypedef(toType: OctoType(kind: .Enum(obj), optional: false, mutable: true), name: name)
+    } else {
+      throw AddTypedefError.typeNotTypedefCapable(obj: object)
+    }
+  }
+
+  public mutating func addTypedef(toType type: OctoType, name: String) {
+    if let existingType = self.typedefs[name] {
+      Self.logger.warning("Typedef with name \(name) already exists for type \(existingType), redefinition will override the current definition with \(type)")
+    }
+    self.typedefs[name] = type
+  }
+
+  //public mutating func addTypedefListener(toTypeResolver: @escaping (OctoLibrary) -> OctoType?, name: String) {
+  //  self.deferredTypedefResolutions.append((name, toTypeResolver))
+  //}
 
   public func renameObjects(_ renameOp: (String) throws -> String) rethrows {
     for object in self.objects {
@@ -59,9 +141,28 @@ public struct OctoLibrary: AutoRemovable {
 
   public mutating func finalize() throws {
     if self.hasFinalized { return }
-    for obj in self.objects {
-      try obj.finalize()
+    let sortOrder = { (obj: OctoObject) in
+      if obj is OctoTypedef { return 1 }
+      if obj is OctoField || obj is OctoArgument || obj is OctoEnumCase { return 2 }
+      if obj is OctoRecord || obj is OctoEnum { return 3 }
+      if obj is OctoFunction { return 4 }
+      return 5
+    }
+    for obj in self.objects.sorted(by: { (a, b) in sortOrder(a) < sortOrder(b) }) {
+      try obj.finalize(self)
     }
     self.hasFinalized = true
+  }
+}
+
+extension OctoLibrary: CustomDebugStringConvertible {
+  public var debugDescription: String {
+    """
+    OctoLibrary:
+      ffiLanguage = \(self.ffiLanguage)
+    \(self.objects.filter { obj in !(obj is OctoField || obj is OctoEnumCase || obj is OctoArgument) }.map { obj in
+      String(reflecting: obj)
+    }.joined(separator: "\n"))
+    """
   }
 }
